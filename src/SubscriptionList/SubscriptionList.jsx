c4c89a7099b9649fa4f7ba92002762070ba77933
Joel Davalos <jsdavalos@gmail.com>
2025-03-12 13:19:15 -0600

    Added return statement that shows list of subs with name, price, and active subs and added a view details button

--- src/SubscriptionList/SubscriptionList.jsx
+++ src/SubscriptionList/SubscriptionList.jsx
@@ -10,4 +10,26 @@ function SubscriptionList() {
       .then(data => setSubscriptions(data))
       .catch(error => console.error('Error fetching subscriptions', error))
   }, [])
+
+  return (
+    <section className="subscription-list">
+      <header className="list-header">
+        <h2>Subscription List</h2>
+      </header>
+
+      <div className="subscriptions">
+        {subscriptions.map(subscription => (
+          <div className="subscription-item" key={subscription.id}>
+            <h3>{subscription.title}</h3>
+            <p>{subscription.description}</p>
+            <p>Price: {subscription.price}</p>
+            <p>Active Customer subs:{subscription.customer_subscribed}</p>
+            <button>View Details</button>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
+
+export default SubscriptionList;
