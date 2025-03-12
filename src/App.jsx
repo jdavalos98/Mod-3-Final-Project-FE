0f3292f5160150778951d5f4d8be09312654d33f
Joel Davalos <jsdavalos@gmail.com>
2025-03-12 13:23:34 -0600

    Added SubscriptionList route in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
-// import SubscriptionList from './SubscriptionList/SubscriptionList'; 
+import SubscriptionList from './SubscriptionList/SubscriptionList'; 
 
 function App() {
   return (
       <section className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/subscriptions" element={<SubscriptionList />} />
 
         </Routes>
       </section>
