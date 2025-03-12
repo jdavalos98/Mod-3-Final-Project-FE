861b4d1852b4f138791943f79e964afea95f148e
Joel Davalos <jsdavalos@gmail.com>
2025-03-12 12:27:02 -0600

    Added return to homepage view

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './HomePage.css'
+
+function HomePage() {
+  return (
+    <section className="home-container">
+      <header className="home-header">
+        <h1>Welcome to the Tea Subscription Service</h1>
+        <p>Your go-to platform for exploring and managing tea subscriptions</p>
+      </header>
+
+      <nav className="home-nav">
+        <Link to="/subscriptions" className="home-link">
+          View All Subscriptions
+        </Link>
+        <Link to="/about" className="home-link">
+          About Us
+        </Link>
+      </nav>
+    </section>
+  )
+}
+
+export default HomePage;
