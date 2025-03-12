ebca10cddcf00c79e224d8df4f53060ab84c5699
Joel Davalos <jsdavalos@gmail.com>
2025-03-12 15:18:39 -0600

    added return statement displaing sub details, customers and there details and teas with there deatils

--- src/SubscriptionDetails/SubscriptionDetails.jsx
+++ src/SubscriptionDetails/SubscriptionDetails.jsx
@@ -20,5 +20,48 @@ function SubscriptionDetail() {
       })
   }, [id])
 
+  if (loading) {
+    return <p>Loading data...</p>
+  }
   
+  const {attributes} = subscription.
+  
+  return (
+    <section className="subscription-detail">
+      <header>
+        <h2>Subscription Details</h2>
+      </header>
+      <div className="subscription-info">
+        <h3>{attributes.title}</h3>
+        <p><strong>Price:</strong> ${attributes.price}</p>
+        <p><strong>Frequency:</strong> {attributes.frequency}</p>
+        <p><strong>Customers Subscribed:</strong>{attributes.customer_subscribed}</p>
+
+        <h4>Customers:</h4>
+        <ul>
+          {attributes.customers.map((customer, index) => (
+            <li key={(index)}>
+              <p><strong>{customer.title}</strong></p>
+              <p>Email: {customer.email}</p>
+              <p>Status: {customer.status ? 'Active' : 'Cancelled'}</p>
+            </li>
+          ))}
+        </ul>
+
+        <h4>Teas:</h4>
+        <ul>
+          {attributes.teas.map((tea, index) => (
+            <li key={index}>
+              <p><strong>{tea.title}</strong></p>
+              <p>{tea.description}</p>
+              <p>Temperature: {tea.temperature}</p>
+              <p>Brew Time: {tea.brew_time} minutes</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
 }
+
+export default SubscriptionDetail;
